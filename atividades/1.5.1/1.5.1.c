85baa9f76ea10fad1fa35e02c17353499690a2d0
Eric Emanuel <ericemanuel@gmail.com>
2022-11-15 18:05:41 -0300

    Refinando atividade 1.5.1

--- atividades/1.5.1/1.5.1.c
+++ atividades/1.5.1/1.5.1.c
@@ -2,36 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define width 200
-#define height 100
+#define title "1.5.1"
+#define width 720
+#define height 480
+#define size 150
+#define cycle 10
 
 int main(int argc, char* args[]) {
     // inicialização
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("1.5.1", SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED, width,
-                                          height, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN);
     SDL_Renderer* render = SDL_CreateRenderer(window, -1, 0);
 
     // execução
-    int mx, my, isEvent, limitX, limitY, wait;
+    int mx, my, isEvent, invertX, invertY, wait, before;
     isEvent = 0;
-    limitX = limitY = 1;
-    wait = 200;
-
-    Uint32 before;
+    invertX = invertY = 1;
+    wait = cycle;
 
     SDL_Color c1 = {255, 0, 0, 0};
     SDL_Color c2 = {0, 255, 0, 0};
     SDL_Color c3 = {0, 0, 255, 0};
 
-    SDL_Rect r1 = {width / 4, height / 2, 10, 10};
-    SDL_Rect r2 = {width / 2, height / 2, 10, 10};
-    SDL_Rect r3 = {3 * width / 4, height / 2, 10, 10};
+    SDL_Rect r1 = {width / 4 - size / 2, height / 2 - size / 2, size, size};
+    SDL_Rect r2 = {width / 2 - size / 2, height / 2 - size / 2, size, size};
 
     SDL_Event event;
     while (1) {
-        SDL_SetRenderDrawColor(render, 255, 255, 255, 0);
+        // redesenho
+        SDL_SetRenderDrawColor(render, 0, 0, 0, 0);
         SDL_RenderClear(render);
 
         SDL_SetRenderDrawColor(render, c1.r, c1.g, c1.b, c1.a);
@@ -41,6 +40,7 @@ int main(int argc, char* args[]) {
         SDL_RenderFillRect(render, &r2);
 
         SDL_GetMouseState(&mx, &my);
+        SDL_Rect r3 = {mx, my, size, size};
         SDL_SetRenderDrawColor(render, c3.r, c3.g, c3.b, c3.a);
         SDL_RenderFillRect(render, &r3);
 
@@ -55,47 +55,47 @@ int main(int argc, char* args[]) {
                     switch (event.key.keysym.sym) {
                         case SDLK_UP:
                             if (r2.y > 0)
-                                r2.y -= 5;
+                                r2.y -= size / 5;
                             break;
                         case SDLK_DOWN:
-                            if (r2.y < (height - r2.h))
-                                r2.y += 5;
+                            if (r2.y < (height - size))
+                                r2.y += size / 5;
                             break;
                         case SDLK_LEFT:
                             if (r2.x > 0)
-                                r2.x -= 5;
+                                r2.x -= size / 5;
                             break;
                         case SDLK_RIGHT:
-                            if (r2.x < (width - r2.w))
-                                r2.x += 5;
+                            if (r2.x < (width - size))
+                                r2.x += size / 5;
                             break;
                     }
                     break;
 
                 case SDL_QUIT:
-                    // finalização
-                    SDL_DestroyRenderer(render);
-                    SDL_DestroyWindow(window);
                     SDL_Quit();
+                    return (0);
             }
 
-            wait -= (SDL_GetTicks() - before);
+            wait -= SDL_GetTicks() - before;
+            if (wait < 0)
+                wait = 0;
 
         } else {
-            if (r1.x >= (width - r1.w))
-                limitX = -1;
+            if (r1.x >= (width - size))
+                invertX = -1;
             else if (r1.x <= 0)
-                limitX = 1;
+                invertX = 1;
 
-            if (r1.y >= (height - r1.h))
-                limitY = -1;
+            if (r1.y >= (height - size))
+                invertY = -1;
             else if (r1.y <= 0)
-                limitY = 1;
+                invertY = 1;
 
-            r1.x += 5 * limitX;
-            r1.y += 5 * limitY;
+            r1.x += size / 20 * invertX;
+            r1.y += size / 20 * invertY;
 
-            wait = 200;
+            wait = cycle;
         }
     }
 }
